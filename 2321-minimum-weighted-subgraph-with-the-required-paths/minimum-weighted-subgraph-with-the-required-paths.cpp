327c98ac95c7a58480a832e9d29128e08038d375
sarthakgoel075 <124894709+sarthakgoel075@users.noreply.github.com>
2025-01-18 23:34:12 +0530

    Time: 285 ms (17.01%) | Memory: 211.6 MB (10.82%) - LeetSync

--- 2321-minimum-weighted-subgraph-with-the-required-paths/minimum-weighted-subgraph-with-the-required-paths.cpp
+++ 2321-minimum-weighted-subgraph-with-the-required-paths/minimum-weighted-subgraph-with-the-required-paths.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
-    vector<long long> fun(int start, vector<vector<pair<long long, long long>>>& adj) {
-        vector<long long> weight(adj.size(), LLONG_MAX);
-        priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<pair<long long, long long>>> pq;
-        weight[start] = 0;
-        pq.push({0, start});
-        
-        while (!pq.empty()) {
-            auto i = pq.top();
-            long long cost = i.first;
-            long long curr = i.second;
-            pq.pop();
-            
-            if (cost > weight[curr]) continue;
-            
-            for (auto nxt : adj[curr]) {
-                long long new_node = nxt.first;
-                long long new_cost = cost + nxt.second;
-                if (new_cost < weight[new_node]) {
-                    weight[new_node] = new_cost;
-                    pq.push({new_cost, new_node});
-                }
+    vector<long long> fun(int start,vector<vector<pair<long long,long long>>>&adj){
+        vector<long long>weight(adj.size(),LLONG_MAX);
+        weight[start]=0;
+       priority_queue<pair<long long,long long>,vector<pair<long long,long long>>,greater<pair<long long,long long>>>pq;
+       pq.push({0,start});
+       while(!pq.empty()){
+        auto i=pq.top();
+        long long cost=i.first;
+        long long curr=i.second;
+        pq.pop();
+         if (cost > weight[curr]) continue;
+        for(auto nxt:adj[curr]){
+            long long new_node=nxt.first;
+            long long new_cost=cost+nxt.second;
+            if(new_cost<weight[new_node]){
+                weight[new_node]=new_cost;
+                pq.push({new_cost,new_node});
             }
         }
-        return weight;
+       }
+       return weight;
     }
-    
     long long minimumWeight(int n, vector<vector<int>>& edges, int src1, int src2, int dest) {
-        vector<vector<pair<long long, long long>>> adj(n), rev_adj(n);
-        
-        for (auto& i : edges) {
-            adj[i[0]].push_back({i[1], i[2]});
-            rev_adj[i[1]].push_back({i[0], i[2]});  // Reverse graph
+        vector<vector<pair<long long,long long>>>adj(n);
+           vector<vector<pair<long long,long long>>>rev_adj(n);
+        for(auto i:edges){
+            adj[i[0]].push_back({i[1],i[2]});
+            rev_adj[i[1]].push_back({i[0], i[2]});
         }
-        
         vector<long long> from_src1 = fun(src1, adj);
         vector<long long> from_src2 = fun(src2, adj);
         vector<long long> from_dest = fun(dest, rev_adj);
