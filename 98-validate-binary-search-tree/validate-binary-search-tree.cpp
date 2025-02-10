3b23242dc7e4620aab3f1983ddc16d5faf9711c6
sarthakgoel075 <124894709+sarthakgoel075@users.noreply.github.com>
2025-02-11 00:02:20 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (19.30%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool helper(TreeNode* root, long minVal, long maxVal) {
+        if (!root) return true;
+        if (root->val <= minVal || root->val >= maxVal) return false;
+        return helper(root->left, minVal, root->val) && helper(root->right, root->val, maxVal);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return helper(root, LONG_MIN, LONG_MAX);
+    }
+};
