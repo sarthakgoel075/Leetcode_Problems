a89ca17a489a9410a085e92727592deafc720160
sarthakgoel075 <124894709+sarthakgoel075@users.noreply.github.com>
2025-04-08 14:59:37 +0530

    Time: 0 ms (100.00%) | Memory: 27.5 MB (77.30%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        vector<int>v(101,-1);
+       vector<int>v(101,-1);
         int n=nums.size();
         int count=0;
         int point=0;
         for(int i=0;i<nums.size();i++){
-            if(point>=n)break;
-            if(v[nums[i]]<point){
+           if(point>i)continue;
+            if(v[nums[i]]==-1){
                 v[nums[i]]=i;
-                continue;
             }
             else if(v[nums[i]]!=-1){
                 while(v[nums[i]]>=point){
