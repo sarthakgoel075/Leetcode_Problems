35114ab0bca65ea3e149d55a83ef96a72fb7a88e
sarthakgoel075 <124894709+sarthakgoel075@users.noreply.github.com>
2025-04-29 15:35:16 +0530

    Time: 475 ms (32.59%) | Memory: 134.8 MB (33.66%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,47 +1,32 @@
 class Solution {
 public:
-    int findparent(vector<int>&parent,int i){
-        if(parent[i]!=i){
-            parent[i]=findparent(parent,parent[i]);
-        }
-        return parent[i];
-    }
     int minCostConnectPoints(vector<vector<int>>& points) {
-        vector<pair<int,pair<int,int>>>arr;
         int n=points.size();
+vector<vector<pair<int,int>>> adj(n);
         for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n;j++){
-                int w=abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
-                arr.push_back({w,{i,j}});
+adj[i].push_back({j, abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])});
+adj[j].push_back({i, abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])});
+               }
             }
-        }
-        sort(arr.begin(),arr.end());
-        vector<int>parent(n);
-        vector<int>rank(n,0);
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-        }
-        int ans=0;
-        for(int i=0;i<arr.size();i++){
-            int w=arr[i].first;
-            int u=arr[i].second.first;
-            int v=arr[i].second.second;
-            int p_u=findparent(parent,u);
-            int p_v=findparent(parent,v);
-            if(p_u!=p_v){
-                if(rank[p_u]>rank[p_v]){
-                    parent[p_v]=p_u;
+         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+         int ans=0;
+         pq.push({0,0});
+         vector<int>visited(n,0);
+         while(!pq.empty()){
+            int w=pq.top().first;
+            int u=pq.top().second;
+            pq.pop();
+            if(visited[u])continue;
+            ans+=w;
+            visited[u]=1;
+            for(auto it:adj[u]){
+                if(!visited[it.first]){
+                    pq.push({it.second,it.first});
                 }
-                else if(rank[p_v]>rank[p_u]){
-                    parent[p_u]=p_v;
-                }
-                else {
-                    parent[p_v]=p_u;
-                    rank[p_u]++;
-                }
-                ans+=w;
             }
+         }
+         return ans;
         }
-        return ans;
-    }
+    
 };
