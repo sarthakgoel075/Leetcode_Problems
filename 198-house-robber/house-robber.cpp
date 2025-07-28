d2578617df6f872fb31c84867ed587664a495d2a
sarthakgoel075 <124894709+sarthakgoel075@users.noreply.github.com>
2025-07-28 11:38:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (22.59%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    int fun(int i,vector<int>&nums,vector<int>&dp){
-       int n=nums.size();
-       if(i>=n)return 0;
-              if(dp[i]!=-1)return dp[i];
-
-       int take=0;
-       int nottake=0;
-       //take case
-        
-         take=nums[i]+fun(i+2,nums,dp);
-           
-        // not take case
-        nottake=fun(i+1,nums,dp);
-        return dp[i]=max(take,nottake);
-       }
-    
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1);
-       return fun(0,nums,dp);
+        int n=nums.size();
+        vector<int>dp(n,0);
+        for(int i=0;i<n;i++){
+          int take=0;
+          if(i>=2){
+            take=nums[i]+dp[i-2];
+          }
+          else{
+            take=nums[i];
+          } 
+
+          int nottake=0;
+          if(i>0)nottake=dp[i-1];
+          dp[i]=max(take,nottake);
+        }
+        return dp[n-1];
     }
 };
