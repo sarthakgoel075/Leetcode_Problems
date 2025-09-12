4b71482f1fb88fa8cf3695f0fad909c21f479504
Sarthak Goel <124894709+sarthakgoel075@users.noreply.github.com>
2025-09-12 10:40:04 +0530

    Time: 1705 ms (13.39%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select customer_id,COUNT(visit_id) as count_no_trans from Visits as v where NOT EXISTS (select 1 from  Transactions t where v.visit_id = t.visit_id) GROUP BY customer_id ORDER BY count_no_trans
