9d361788c23e5abeda096fd469066367f6c7f018
Sarthak Goel <124894709+sarthakgoel075@users.noreply.github.com>
2026-06-13 02:42:40 +0530

    Time: 8 ms (18.12%) | Memory: 21.2 MB (18.42%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,58 +1,21 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        bool firstrowzero=false;
-        bool firstcolzero=false;
-        for(int i=0;i<n;i++){
-            if(matrix[i][0]==0){
-                firstcolzero=true;
-                break;
-            }
-        }
-           for(int i=0;i<m;i++){
-            if(matrix[0][i]==0){
-                firstrowzero=true;
-                break;
-            }
-        }
-        //first row and first col use as an indentify that which row and col has to be zero
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-               if(matrix[i][j]==0){
-                   matrix[0][j]=0;
-                   matrix[i][0]=0;
-               }
-            }
-        }
-        // now make changes in matrix and make  row and col as zero acc to first row and col
-        for(int i=1;i<n;i++){
-            if(matrix[i][0]==0){
-                for(int j=0;j<m;j++){
-                    matrix[i][j]=0;
+        set<int>row;
+        set<int>col;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                if(matrix[i][j]==0){
+                    row.insert(i);
+                    col.insert(j);
                 }
             }
         }
-
-        for(int i=1;i<m;i++){
-            if(matrix[0][i]==0){
-                for(int j=0;j<n;j++){
-                    matrix[j][i]=0;
-                }
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                if(row.contains(i))matrix[i][j]=0;
+                if(col.contains(j))matrix[i][j]=0;
             }
-        }
-
-        // now check for first row and col 
-        if(firstrowzero){
-            for(int i=0;i<m;i++){
-                matrix[0][i]=0;
-            }
-        }
-        if(firstcolzero){
-            for(int i=0;i<n;i++){
-                matrix[i][0]=0;
-            }
-        }
+    }
     }
 };
